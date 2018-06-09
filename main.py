0a8cdf6daefff5592d9250c3458b6ed53a959615
michael0703 <b05902059@ntu.edu.tw>
2018-06-09 21:26:07 +0800

    'login'

---
+++ main.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+FB_Url = 'https://facebook.com'
+
+class  ClubManage():
+	def __init__(self, account, passwd):
+		self.account = account
+		self.passwd = passwd
+
+	def login(self):
+		driver = webdriver.Chrome()
+		driver.get(FB_Url)
+		driver.find_element_by_name("email").send_keys(self.account)
+		driver.find_element_by_name("pass").send_keys(self.passwd)
+		driver.find_element_by_xpath("//label[@id='loginbutton']/input").click()
+
+if __name__ == '__main__':
+	print("xxxx")
+	ClubManage('0972795235', 'weiskewer0703').login()
+		
+
+
+		
+	
