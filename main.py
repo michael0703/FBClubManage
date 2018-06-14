a5e7143730131751613580cd9c5c6527e0673ba9
michael0703 <b05902059@ntu.edu.tw>
2018-06-14 23:31:54 +0800

    '0614-2'

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.action_chains import ActionChains
 from lxml import etree, html
 import sys
+import time
 
 FB_Url = 'https://facebook.com'
 Club_Url = 'https://www.facebook.com/groups/huberstudents/'
@@ -13,7 +14,6 @@ class  ClubManage():
 		self.passwd = passwd
 		self.driver = webdriver.Chrome()
 	def login(self):
-		
 		self.driver.get(FB_Url)
 		self.driver.find_element_by_name("email").send_keys(self.account)
 		self.driver.find_element_by_name("pass").send_keys(self.passwd)
@@ -21,19 +21,16 @@ class  ClubManage():
 	def EnterClub(self, Url):
 		self.driver.get(Url);
 	def SearchPost(self):
-		#root = etree.fromstring(self.driver.page_source, etree.HTMLParser());
-		#print("=====")
-		#print(len(root.xpath("//div[@class='_5pcr userContentWrapper']")))
-		#posts = self.driver.find_elements_by_css_selector("[class = "_1dwg _1w_m _q7o"]")
-		posts = self.driver.find_elements_by_xpath("//*[@id='comment_js_jc']/div/div/div/div[2]/div/div/div[1]/div[1]/div/span")
-		print(len(posts))
-		'''
+		posts = self.driver.find_elements_by_xpath("//div[@class='_5pcr userContentWrapper']")
+		#xpath = "//div[@class='_5pcr userContentWrapper']//span[@class='UFICommentActorAndBody']"
+		time.sleep(2)
+		print(len(posts), "======")
 		for post in posts:
-			resp=[]
-			resp = post.find_elements_by_xpath("//span[@class='UFICommentActorAndBody']")
-			print(len(resp))
-			print("========")
-		'''
+			target = post.find_elements_by_xpath("//span[@class='UFICommentActorAndBody']")
+			time.sleep(10)
+			print(len(target), "======", sep='\n')
+
+
 if __name__ == '__main__':
 	if len(sys.argv) <= 2:
 		print("Not Enough Arguments: python main.py ACCOUNT PASSWD!")
